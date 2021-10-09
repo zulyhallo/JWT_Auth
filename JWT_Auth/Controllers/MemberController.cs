f9e9c01d1e5bf804d21611c082d2706a1523323e
zulyhallo <zuleyhaokur54@gmail.com>
2021-10-09 07:25:47 +0300

    Add project files.

---
+++ JWT_Auth/Controllers/MemberController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using JWT_Auth.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JWT_Auth.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        private readonly IJwtAuth jwtAuth;
+        private readonly List<Member> lstMember = new List<Member>()
+        {
+            new Member{Id=1, Name="Kirtesh" },
+            new Member {Id=2, Name="Nitya" },
+            new Member{Id=3, Name="pankaj"}
+        };
+        public MemberController(IJwtAuth jwtAuth)
+        {
+            this.jwtAuth = jwtAuth;
+        }
+        // GET: api/<MemberController>
+        [HttpGet]
+        public IEnumerable<Member> AllMembers()
+        {
+            return lstMember;
+        }
+
+
+        // GET api/<MemberController>/5
+        [HttpGet("{id}")]
+        public Member MemberByid(int id)
+        {
+            return lstMember.Find(x => x.Id == id);
+        }
+
+        [AllowAnonymous]
+        // POST api/<MemberController>
+        [HttpPost("authentication")]
+        public IActionResult Authentication([FromBody] UserCredential userCredential)
+        {
+            var token = jwtAuth.Authentication(userCredential.UserName, userCredential.Password);
+            if (token == null)
+                return Unauthorized();
+            return Ok(token);
+        }
+
+        
+        // PUT api/<MemberController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MemberController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
